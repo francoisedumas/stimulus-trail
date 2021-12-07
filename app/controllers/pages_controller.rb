2abf8b3c1876fd2e08f7fe2f3b1832a7b24e42e6
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-12-07 18:13:39 +0100

    Day 2 submit form on change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    if params[:date].present?
+      @selected_date = params[:date]
+    else
+      @selected_date = "Select a day"
+    end
   end
 end
